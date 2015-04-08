34bd5dc70445991f86c89533201836131302a27c
Kim Taylor <kim.taylor4@live.vu.edu.au>
2015-04-08 20:28:48 +1000

    State machine finished

--- src/omxplayer.h
+++ src/omxplayer.h
@@ -1,1 +1,34 @@
-extern int omxplay_event_loop(int argc, char *argv[]);
+class OMXPlayerInterface {
+    typedef int (*callback_func_t)(OMXReader *reader);
+
+    public:
+	OMXPlayerInterface() {
+	    callback_func = NULL;
+	    loop_func = NULL;
+	}
+
+	void set_callback(callback_func_t func) {
+	    callback_func = func;
+	}
+
+	void set_loop_callback(callback_func_t func) {
+	    loop_func = func;
+	}
+
+	static OMXPlayerInterface *get_interface();
+	int omxplay_event_loop(int argc, char *argv[]);
+
+    private:
+	int control_callback(OMXReader *reader) {
+	    if (callback_func) return callback_func(reader);
+	    return 0;
+	}
+
+	int loop_callback(OMXReader *reader) {
+	    if (loop_func) return loop_func(reader);
+	    return 0;
+	}
+
+	callback_func_t callback_func;
+	callback_func_t loop_func;
+};
